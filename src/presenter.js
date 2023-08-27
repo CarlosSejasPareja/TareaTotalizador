f3e8279da045814fc408a426005ae673d4e2d5dd
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-08-27 16:02:02 -0400

    muestra cantidad y precio de items

---
+++ src/presenter.js
@@ -0,0 +1,14 @@
+//import metodos from "./totalizador";
+
+const cantidad_items = document.querySelector("#cant-items");
+const precio_item = document.querySelector("#precio-item")
+const form = document.querySelector("#tot-form");
+const div = document.querySelector("#resultado-div");
+
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  div.innerHTML = "<p>" + "Cantidad: " + cantidad_items.value + "<br> Precio: " + precio_item.value +"</p>" 
+  
+});
